e10ae446628b4070871f74ce2c326955e96b6ab1
dexer <norton@e1.ru>
2021-09-01 21:40:15 +0500

    update function_timer_deco logic

--- task2.py
+++ task2.py
@@ -1,16 +1,17 @@
 from threading import Thread
 from time import sleep
+import time
 
 """
 ########## first example #############
-@function_timer_deco(5)
+@function_timer_deco
 def sum(a, b):
     return a + b
 
 print('result', sum(1, 2))
 
 ########## second example #############
-@function_timer_deco(5)
+@function_timer_deco
 def sum(a, b):
     sleep(30)
     return a + b
@@ -19,41 +20,28 @@ print('result', sum(1, 2))
 """
 
 
-def start_error_timer(seconds_count, result, fn_name):
-    sleep(seconds_count)
-    result['error'] = TimeoutError(
-        'Times up! Function "{}" did not return a result after {} seconds'.format(fn_name, seconds_count))
-
-
-def get_main_fn_value(fn, result):
+def function_timer_deco(fn):
     def inner(*args, **kwargs):
-        try:
-            result['value'] = fn(*args, **kwargs)
-        except Exception as e:
-            result['error'] = e
+        TIMER_VALUE = 1
+        results = []
+
+        def get_main_fn_value():
+            try:
+                results.append(fn(*args, **kwargs))
+            except BaseException as e:
+                results.append(e)
+
+        tr = Thread(target=get_main_fn_value, daemon=True)
+        tr.start()
+        tr.join(timeout=TIMER_VALUE)
+
+        results.append(TimeoutError(
+            f'Times up! Function "{fn.__name__}" did not return a result after {TIMER_VALUE} seconds'))
+
+        result, *_ = results
+
+        if isinstance(result, BaseException):
+            raise result
+        return result
+
     return inner
-
-
-def function_timer_deco(seconds_count):
-    def wrapper(fn):
-        def inner(*args, **kwargs):
-            result = {'value': None,
-                      'error': None}
-            timer_thread = Thread(target=start_error_timer, args=(
-                seconds_count, result, fn.__name__), daemon=True)
-            main_fn_thread = Thread(target=get_main_fn_value(fn, result), args=args,
-                                    kwargs=kwargs, daemon=True)
-
-            timer_thread.start()
-            main_fn_thread.start()
-
-            while True:
-                value = result['value']
-                error = result['error']
-                if error:
-                    raise error
-                elif value:
-                    return result['value']
-
-        return inner
-    return wrapper
