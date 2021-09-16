e63f81089e55704cb638ab7f7a93eb5db5ecbc91
dexer <norton@e1.ru>
2021-09-16 21:12:53 +0500

    update task5

--- task5.py
+++ task5.py
@@ -1,45 +1,90 @@
-class Macbook():
-    PORT_COUNT = 4
+from abc import ABC, abstractmethod
+
+
+class MacDevice():
     PORT_TYPE = 'usb-c'
 
     def __init__(self):
-        self.device_list = []
-        self.connected_port_count = 0
+        self.currentConnection = None
 
-    def connect(self, device):
-        device_name = device['name']
-        port_type = device['port_type']
+    def connect(self, macName):
+        self.currentConnection = macName
 
-        if self.connected_port_count == self.PORT_COUNT:
-            raise ValueError(
-                'The maximum number of connections has been reached')
-        elif device_name not in self.device_list:
-            raise ValueError('This device is not supported by this mac')
-        elif port_type != self.PORT_TYPE:
-            raise ValueError(
-                f"Port {port_type} is not supported by this mac")
-
-        self.connected_port_count += 1
-        print('connected')
-
-    def disconnect(self):
-        if self.connected_port_count == 0:
-            raise AttributeError('This device has no connections')
-        self.connected_port_count -= 1
-        print('disconnected')
-
-    def add_device(self, device_name):
-        if device_name in self.device_list:
-            print(f'device {device_name} already added')
-        self.device_list.append(device_name)
-        print(f'device added {device_name}')
+    def checkConnection(self):
+        if self.currentConnection is None:
+            raise ValueError('Device is not connected to mac')
 
 
-mac1 = Macbook()
-mac1.add_device('device_name')
-mac1.connect({'name': 'device_name', 'port_type': 'usb-c'})
-print(mac1.connected_port_count)
-mac2 = Macbook()
-mac2.add_device('device_name')
-mac2.connect({'name': 'device_name', 'port_type': 'usb-c'})
-print(mac2.connected_port_count)
+class KeyBoard(MacDevice):
+    def print(self, value):
+        self.checkConnection()
+        print(f'Text from keyboard: {value}')
+
+
+class Mouse(MacDevice):
+    def mouseMove(self):
+        self.checkConnection()
+        print('Mouse moved')
+
+
+class AbstractDisplay(ABC, MacDevice):
+    @abstractmethod
+    def show(self): pass
+
+
+class HDDisplay(AbstractDisplay):
+    def show(self):
+        self.checkConnection()
+        print('Broadcast from HDDisplay')
+
+
+class VGADisplay(AbstractDisplay):
+    def show(self):
+        self.checkConnection()
+        print('Broadcast from VGADisplay')
+
+
+class AbstractMac(ABC):
+    PORT_TYPE = 'usb-c'
+
+    def __init__(self):
+        self.ports = {
+            'port1': None,
+            'port2': None,
+            'port3': None,
+            'port4': None,
+        }
+
+    @abstractmethod
+    def connect(self, device: MacDevice): pass
+
+
+class Macbook(AbstractMac):
+
+    def connect(self, device: MacDevice):
+        if device.PORT_TYPE != self.PORT_TYPE:
+            raise ValueError('Wrong port type')
+
+        for portName in self.ports.keys():
+            if self.ports[portName] is None:
+                print(f"connected {device.__class__.__name__} to {portName}")
+                self.ports[portName] = device
+                device.connect(self.__class__.__name__)
+                break
+        else:
+            raise ValueError('No free slots')
+
+#example
+# mac = Macbook()
+# first_display = HDDisplay()
+# second_display = VGADisplay()
+# mouse = Mouse()
+# keyboard = KeyBoard()
+# mac.connect(first_display)
+# mac.connect(second_display)
+# mac.connect(mouse)
+# mac.connect(keyboard)
+# first_display.show()
+# second_display.show()
+# mouse.mouseMove()
+# keyboard.print('text')
